c33d54a0f88256c0494021f9fa54e41cc5fd1875
Somnath Halder <somnathhalder328@gmail.com>
2021-02-10 16:22:05 +0530

    First-Commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const fetch = require("node-fetch")
+const app = express()
+const port = 4000
+
+const WeekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"]
+let arr
+const Time = 1000 * 60 * 60
+
+function NewDate(SomeDate) {
+  let Current = new Date().toISOString()
+  let CurrentSplit = Current.split("T")
+  let AnimeTime = SomeDate.split("T")
+  if (CurrentSplit[1] > AnimeTime[1]) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+function CheckDate(ArrayOfAnime) {
+  let HasDropped = []
+  for (let i = 0; i < ArrayOfAnime.length; i++) {
+    if (NewDate(ArrayOfAnime[i].airing_start)) {
+        HasDropped.push(ArrayOfAnime[i])
+    }
+  }
+  return HasDropped
+}
+
+async function FetchData() {
+  let Day = WeekDays[new Date().getDay() - 1]
+  let data = await (await fetch(`https://api.jikan.moe/v3/schedule/${Day}`)).json()
+  return data[Day.toLowerCase()]
+}
+
+arr = FetchData()
+
+setInterval(() => {
+  arr = FetchData()
+}, Time)
+
+app.get("/", (req, res) => {
+  if (arr === undefined) {
+    res.send("Not Available")
+  } else {
+    arr.then(data => {
+      let Dropped = CheckDate(data)
+      res.send(Dropped)
+    })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
